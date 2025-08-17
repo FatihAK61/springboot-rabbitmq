daeecf3c8f50ff30f1f1e202fea301ee3dab18ed
FatihAK61 <fatihak61@gmail.com>
2025-08-17 10:36:18 +0300

    Update

--- src/main/java/com/rabbit/springboot/starter/RabbitmqApplication.java
+++ src/main/java/com/rabbit/springboot/starter/RabbitmqApplication.java
@@ -2,8 +2,10 @@ package com.rabbit.springboot.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rabbit.springboot"})
 public class RabbitmqApplication {
 
     public static void main(String[] args) {
