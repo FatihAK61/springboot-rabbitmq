958fa020516e28e86794da66457a3d9e40549bd6
FatihAK61 <fatihak61@gmail.com>
2025-08-17 08:52:31 +0300

    Update

--- src/main/java/com/rabbit/springboot/starter/RabbitmqApplication.java
+++ src/main/java/com/rabbit/springboot/starter/RabbitmqApplication.java
@@ -1,4 +1,4 @@
-package com.rabbit.springboot;
+package com.rabbit.springboot.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
 
 }
--- src/test/java/com/rabbit/springboot/RabbitmqApplicationTests.java
+++ src/test/java/com/rabbit/springboot/RabbitmqApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RabbitmqApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
