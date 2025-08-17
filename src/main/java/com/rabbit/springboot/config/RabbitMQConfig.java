e49b9d6239d67e274bd7340a4500c68e216de41c
FatihAK61 <fatihak61@gmail.com>
2025-08-17 09:47:02 +0300

    Update

---
+++ src/main/java/com/rabbit/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package com.rabbit.springboot.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value( "${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value( "${rabbitmq.exchange.name}")
+    private String exchange;
+
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    public TopicExchange exchange() {
+    	return new TopicExchange(exchange);
+    }
+
+}
